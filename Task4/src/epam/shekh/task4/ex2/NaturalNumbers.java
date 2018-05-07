f91b55c9b69eac18a24e0e03be6c26d4f619a17c
stshekh <stshekh@users.noreply.github.com>
2018-05-07 18:53:39 +0300

    Task4(completed+several changes)

--- Task4/src/epam/shekh/task4/ex2/NaturalNumbers.java
+++ Task4/src/epam/shekh/task4/ex2/NaturalNumbers.java
@@ -5,7 +5,11 @@ public class NaturalNumbers {
     private static final int ONEDIGIT = 10;
 
     public NaturalNumbers(int num) {
-        this.num = num;
+        if (num > 0) {
+            this.num = num;
+        } else {
+            this.num = -num;
+        }
     }
 
     private boolean posCheck() {
@@ -19,9 +23,9 @@ public class NaturalNumbers {
     public int maxNum() {
         int natNumber = getNum();
         int max = -1;
-        if (!posCheck()) {
-            return 0;
-        }
+//        if (!posCheck()) {
+//            return 0;
+//        }
         while (natNumber > 0) {
             if (natNumber % ONEDIGIT > max) {
                 max = natNumber % ONEDIGIT;
@@ -32,7 +36,7 @@ public class NaturalNumbers {
     }
 
     public boolean polyCheck() {
-        if (!posCheck()) return false;
+        //if (!posCheck()) return false;
         boolean isOk;
         int nat = getNum();
         int newNat = 0;
@@ -53,7 +57,7 @@ public class NaturalNumbers {
     public boolean isSimple(int nat) {
 
 
-        if (!posCheck()) return false;
+        //if (!posCheck()) return false;
         for (int i = 2; i < nat + 1; i++) {
             if (nat % i == 0 && i != nat) {
                 return false;
@@ -84,41 +88,41 @@ public class NaturalNumbers {
         }
     }
 
-    public static int leastCommonMultiple(int a, int b){
-        return (a*b)/maxCommonDiv(a,b);
+    public static int leastCommonMultiple(int a, int b) {
+        return (a * b) / maxCommonDiv(a, b);
     }
 
-    public int difNumbers(){
-        int nat=getNum();
-        int newNat, numCount=0;
+    public int difNumbers() {
+        int nat = getNum();
+        int newNat, numCount = 0;
         int curNum;
-        for (int i = 0; i <10 ; i++) {
-            newNat=nat;
-            while(newNat>0){
-               curNum=newNat%ONEDIGIT;
-               if(curNum==i){
-                   numCount++;
-                   break;
-               }
-               newNat/=ONEDIGIT;
+        for (int i = 0; i < 10; i++) {
+            newNat = nat;
+            while (newNat > 0) {
+                curNum = newNat % ONEDIGIT;
+                if (curNum == i) {
+                    numCount++;
+                    break;
+                }
+                newNat /= ONEDIGIT;
             }
 
         }
-        if(numCount==1){
+        if (numCount == 1) {
             System.out.println("This numbrer has many equal figures");
         }
         return numCount;
     }
 
-    public boolean perfectNumber(){
-        int nat=getNum();
-        int perfSum=0;
+    public boolean perfectNumber() {
+        int nat = getNum();
+        int perfSum = 0;
 
-        for (int i = 1; i <nat ; i++) {
-            if(nat%i==0){
-                perfSum+=i;
+        for (int i = 1; i < nat; i++) {
+            if (nat % i == 0) {
+                perfSum += i;
             }
         }
-        return (nat==perfSum);
+        return (nat == perfSum);
     }
 }
