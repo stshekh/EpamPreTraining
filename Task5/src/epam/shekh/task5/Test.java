ba9f8443eb4e21aff7712e13be8532f0c43fb1c2
stshekh <i4dsgf8s>
2018-05-08 11:59:46 +0300

    Task5(need corrections)

--- Task5/src/epam/shekh/task5/Test.java
+++ Task5/src/epam/shekh/task5/Test.java
@@ -10,7 +10,8 @@ public class Test {
         System.out.println("Is it sorted? "+array.isSorted());
         System.out.println("Position of local minimum is "+array.localMin());
         System.out.println("Position of local maximum is "+array.localMax());
-        array.revMass();
+
+        System.out.println("minimum "+array.minFinder());
         System.out.println(array.sumParts());
     }
 }
